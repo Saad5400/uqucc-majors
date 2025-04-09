44af9f287e83383347d8ce24d0dfad81fa1ab8fd
Saad Batwa <86385454+Saad5400@users.noreply.github.com>
2025-04-09 16:50:44 +0300

    updated prod url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'http://uqucis-majors.sb.sa',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
