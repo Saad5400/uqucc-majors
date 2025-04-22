1182af97310ebaa1582c94bdde06342c6851599a
itsanas121 <161168604+itsanas121@users.noreply.github.com>
2025-04-22 23:27:25 +0300

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,52 +1,155 @@
-module.exports = {
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
   title: 'تخصصات كلية الحاسبات',
-  tagline: 'تعرف على تخصصات كلية الحاسبات بجامعة أم القرى',
+  tagline: 'تعرف ع تخصصات كلية الحاسبات بجامعة أم القرى',
   favicon: 'img/favicon.png',
 
-  url: 'https://uqucc-majors.sb.sa',
+  // Set the production url of your site here
+  url: 'http://uqucis-majors.sb.sa',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-  
-  organizationName: 'Saad5400',
-  projectName: 'uqucis-majors',
-  
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Saad5400', // Usually your GitHub org/user name.
+  projectName: 'uqucis-majors', // Usually your repo name.
+
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ar',
+    locales: ['ar'],
+  },
+
   presets: [
     [
-      '@docusaurus/preset-classic',
+      'classic',
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),  // تأكد من المسار الصحيح للسايدبار
-          editUrl: 'https://github.com/Saad5400/uqucis-majors/tree/main/',
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/Saad5400/uqucis-majors/tree/main/',
         },
         blog: {
-          blogSidebarTitle: 'كل المقالات',
+          blogSidebarTitle: 'كل المقالات', 
+          blogSidebarCount: 'ALL',  
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/Saad5400/uqucis-majors/tree/main/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+          blogSidebarTitle: 'آخر المقالات',
           blogSidebarCount: 'ALL',
-          editUrl: 'https://github.com/Saad5400/uqucis-majors/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
         },
-      },
+      } satisfies Preset.Options,
     ],
   ],
 
   themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: false,
+    },
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'تخصصات كلية الحاسبات',
       logo: {
         alt: 'My Site Logo',
         src: 'img/favicon.png',
+        href: '/docs/intro'
       },
       items: [
-        { to: '/blog', label: 'المدونة', position: 'left' },
-        { href: 'https://github.com/Saad5400/uqucis-majors', label: 'GitHub', position: 'right' },
+        // {
+        //   type: 'docSidebar',
+        //   sidebarId: 'tutorialSidebar',
+        //   position: 'left',
+        //   label: 'Tutorial',
+        // },
+        // {to: '/blog', label: 'Blog', position: 'left'},
+        // {
+        //   href: 'https://github.com/Saad5400/uqucis-majors',
+        //   label: 'GitHub',
+        //   position: 'right',
+        // },
       ],
     },
     footer: {
       style: 'light',
-      copyright: `جميع الحقوق محفوظة © ${new Date().getFullYear()} فريق مبادرة اختيار التخصص`,
+      // links: [
+      //   {
+      //     title: 'Docs',
+      //     items: [
+      //       {
+      //         label: 'Tutorial',
+      //         to: '/docs/intro',
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: 'Community',
+      //     items: [
+      //       {
+      //         label: 'Stack Overflow',
+      //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+      //       },
+      //       {
+      //         label: 'Discord',
+      //         href: 'https://discordapp.com/invite/docusaurus',
+      //       },
+      //       {
+      //         label: 'X',
+      //         href: 'https://x.com/docusaurus',
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: 'More',
+      //     items: [
+      //       {
+      //         label: 'Blog',
+      //         to: '/blog',
+      //       },
+      //       {
+      //         label: 'GitHub',
+      //         href: 'https://github.com/facebook/docusaurus',
+      //       },
+      //     ],
+      //   },
+      // ],
+      // copyright: Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.,
+      copyright: جميع الحقوق محفوظة © ${new Date().getFullYear()} فريق مبادرة اختيار التخصص,
     },
-  },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
 };
+
+export default config;
