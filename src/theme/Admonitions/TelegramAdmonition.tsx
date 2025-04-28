63c925ff1bc5435ddbb30ccfafbab40fcb654f79
itsanas121 <161168604+itsanas121@users.noreply.github.com>
2025-04-29 00:36:54 +0300

    Update tele link.tsx

--- src/theme/Admonitions/TelegramAdmonition.tsx
+++ src/theme/Admonitions/TelegramAdmonition.tsx
@@ -5,7 +5,7 @@ import { FaTelegram } from "react-icons/fa";
 export default function TelegramAdmonition(props) {
   return <Admonition
     icon={<FaTelegram />}
-    title={<a href="https://t.me/+5M8kuV7aAsRlNDU0" target="_blank" rel="noopener noreferrer">انضم لمجتمع الحملة</a>}
+    title={<a href="https://t.me/uquccmajors" target="_blank" rel="noopener noreferrer">انضم لمجتمع الحملة</a>}
     {...props}
   />;
 }
